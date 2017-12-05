f2c7ab15e513f3964b61bc78ad2d1f95167c72ff
Casey Zimmerman <caseyezimmerman@gmail.com>
2017-12-05 13:20:32 -0500

    connect

--- src/containers/Students.js
+++ src/containers/Students.js
@@ -8,12 +8,31 @@ import { connect } from 'react-redux'
 
 class Students extends Component{
 	render(){
+		console.log(this.props.students)
+		var studentArray = this.props.students.map((student,index)=>{
+			return(<li key={index}>{student.name} sits in seat {student.seat} in the {student.row}nd row</li>)
+		});
 		return(
-			<h1>Students Component</h1>
+			<div>
+				<h1>Students Component</h1>
+				{studentArray} {/*the results of mapping through each invididual element*/ }
+			</div>
 		)
+	}
+}
+
+// we need a fuction to map the redux state to THIS components props
+function mapStateToProps(state){
+	// the state parameter is the root reducer
+	// this function will return an object
+	// property will be the prop in THIS component 
+	// the value will be the piece of state from the reducer
+	return{
+		// this will create a this.props.studets
+		students: state.students
 	}
 }
 
 // in containers(Components that need to know about redux), we dont export the component...
 // we export connect. We pass connect a function and we pass the component
-export default connect()(Students)
+export default connect(mapStateToProps)(Students)
--- src/reducers/StudentReducer.js
+++ src/reducers/StudentReducer.js
@@ -1,22 +1,27 @@
 // Reducers are functions that return a piece of state
 
 export default function(){
+	// an array ob objects
 	return[
 		{
 			name: "Jason",
-			seat: 5
+			seat: 5,
+			row: 2
 		},
 		{
 			name: "Chris",
-			seat: 6
+			seat: 6,
+			row: 2
 		},
 		{
 			name: "Casey",
-			seat: 13
+			seat: 13,
+			row: 2
 		},
 		{
 			name: "Jamie",
-			seat: 12
+			seat: 12,
+			row: 2
 		}
 	]
 }
